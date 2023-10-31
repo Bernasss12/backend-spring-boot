9481571b724ec34c27ac0dc1d2faed3a2e7ff2d5
bantunes <bernardo-manuel.antunes@capgemini.com>
2023-10-31 15:42:58 +0000

    Full CRUD Backend implemented.

---
+++ src/main/java/dev/bernasss12/backend/controller/EmployeeController.java
@@ -0,0 +1,63 @@
+package dev.bernasss12.backend.controller;
+
+import dev.bernasss12.backend.exception.ResourceNotFoundException;
+import dev.bernasss12.backend.model.Employee;
+import dev.bernasss12.backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = { "http://localhost:4200/" })
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAll() {
+        return employeeRepository.findAll();
+    }
+
+    @PostMapping("/employees")
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        return ResponseEntity.ok(
+                employeeRepository
+                        .findById(id)
+                        .orElseThrow(
+                                () -> new ResourceNotFoundException("No employee with id: " + id)
+                        )
+        );
+    }
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable long id, @RequestBody Employee employee) {
+        Employee existingEmployee = employeeRepository
+                .findById(id)
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("No employee with id: " + id)
+                );
+        existingEmployee.setFirstName(employee.getFirstName());
+        existingEmployee.setLastName(employee.getLastName());
+        existingEmployee.setEmailId(employee.getEmailId());
+
+        return ResponseEntity.ok(
+                employeeRepository.save(existingEmployee)
+        );
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Long> deleteEmployeeById(@PathVariable long id) {
+        employeeRepository.deleteById(id);
+        return ResponseEntity.ok(id);
+    }
+
+}
---
+++ src/main/java/dev/bernasss12/backend/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package dev.bernasss12.backend.repository;
+
+import dev.bernasss12.backend.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
